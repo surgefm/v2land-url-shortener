931a952f321ec9a34b94331e8c804ade25c58e74
Xiaoxing Ye <ye@xiaoxing.us>
2018-11-25 02:15:40 +0800

    add: use sqlite as dev db

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import { routers } from './routers/index';
+import sequelize = require('sequelize');
 
 const app = new Koa();
+import db from './models/index';
 
 app
   .use(routers.routes())
   .use(routers.allowedMethods())
   .use(bodyParser());
+
+db
+  .sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err: sequelize.Errors) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = app;
