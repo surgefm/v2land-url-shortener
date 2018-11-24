3a56bebeed713da0c314688ff8e3868aebfa634b
Xiaoxing Ye <ye@xiaoxing.us>
2018-11-25 01:34:51 +0800

    del: unused comments

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ import { routers } from './routers/index';
 
 const app = new Koa();
 
-// router.get('/:id', async (ctx: Koa.Context) => {
-//   ctx.body = 'Hello World!';
-// });
-
 app
   .use(routers.routes())
   .use(routers.allowedMethods())
