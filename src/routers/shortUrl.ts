515232185047f572e92ea20c4fa35ff70112cca3
Xiaoxing Ye <ye@xiaoxing.us>
2018-12-09 00:17:38 +0800

    merge: new route and exist route

---
+++ src/routers/shortUrl.ts
@@ -0,0 +1,42 @@
+import Koa from 'koa';
+import {} from 'koa-bodyparser';
+import Router from 'koa-router';
+import { generateId, generateError } from '../utils';
+import { newUrl, getUrl } from '../controllers/url';
+
+const router = new Router();
+
+const routers = router
+  .post(':shortUrl*', async (ctx: Koa.Context) => {
+    const shortUrl = ctx.params.hasOwnProperty('shortUrl') && ctx.params.shortUrl !== undefined ? ctx.params.shortUrl : generateId();
+
+    const postData: {url?: string} | null | undefined = ctx.request.body;
+    if (postData !== undefined && postData !== null && postData.hasOwnProperty('url') && postData.url !== undefined) {
+      try {
+        await newUrl(shortUrl, postData.url);
+        ctx.body = shortUrl;
+        console.info('New shortUrl:', shortUrl, postData.url);
+      } catch (error) {
+        console.log(error);
+        generateError(ctx, error);
+      }
+    } else {
+      generateError(ctx, 'URL not found in post data.');
+    }
+  })
+  .get(':shortUrl', async (ctx: Koa.Context) => {
+    console.info('Trying to get', ctx.params.shortUrl);
+    const shortUrl = ctx.params.shortUrl;
+    try {
+      const url = await getUrl(shortUrl);
+      ctx.redirect(url);
+    } catch (error) {
+      console.log(error);
+      generateError(ctx, error);
+    }
+  })
+  .get('', async (ctx: Koa.Context) => {
+    ctx.redirect('https://langchao.org');
+  });
+
+export default routers;
