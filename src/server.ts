eb1cc6887003217f91897f4d2ce3ad80db3e4984
Xiaoxing Ye <ye@xiaoxing.us>
2018-12-09 00:17:58 +0800

    add: environment log

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@ const app = require('./app');
 
 app.listen(3000);
 
-console.log('Server running on port 3000');
+console.info('Server running on port 3000');
+console.info('Environment:', process.env.NODE_ENV || 'development')
+;
