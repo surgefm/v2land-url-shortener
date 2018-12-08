7c8fac7570f9bfcdde98a8deaaa6d747d24b77bf
Xiaoxing Ye <ye@xiaoxing.us>
2018-12-09 00:17:15 +0800

    update: a better redis factory

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import Koa from 'koa';
+import * as redis from 'redis';
+
 /**
  * @returns {string} short id
  */
@@ -6,5 +9,39 @@ function generateId() : string {
   const hashids = new Hashids('', 0, 'abcdefghijklmnopqrstuvwxyz0123456789');
   return hashids.encode(Math.floor(Math.random() * Math.floor(100)), Math.floor(Date.now() / 1000 - 1543017600 /* time of Nov 25, 2018 */));
 }
+/**
+ * Generate error
+ * @param  {Koa.Context} ctx
+ * @param  {''} message
+ */
+function generateError(ctx: Koa.Context, message: string) {
+  ctx.body = {
+    'error': true,
+    'message': message,
+    'request': ctx.request.URL,
+  };
+}
 
-export { generateId };
+/**
+ * Init a new redis client
+ */
+function redisFactory() : Promise<redis.RedisClient> {
+  return new Promise<redis.RedisClient>((resolve, reject) => {
+    try {
+      const env = process.env.NODE_ENV || 'development';
+      const redisConfig = require(__dirname + '/config.json')[env].redis;
+      const instance: redis.RedisClient = redis.createClient({
+        url: redisConfig.url,
+        retry_strategy(options: redis.RetryStrategyOptions): number | Error {
+          reject('[Redis] Not able to create client, reason: ' + options.error.message);
+          return options.error;
+        } });
+      instance.on('connect', () => {
+        resolve(instance);
+      });
+    } catch (error) {
+      reject('[Redis] Unknown exception during client creation, reason: ' + error.message);
+    }
+  });
+}
+export { generateId, generateError, redisFactory };
